76966e6dfbc302a14ad647016068c62313443262
Your Name <you@example.com>
2020-01-24 15:06:22 -0800

    changes in onclickhandler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import data from "./data.json";
+import ImageCarousel from "./ImageCarousel";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let groups = Object.keys(data).map(o => data[o]);
+groups = groups[0].groups;
+console.log(groups);
+
+let res = groups.map(i => {
+  for (let j in i) {
+    if (j === "price" || j === "priceRange") {
+      for (let k in i[j]) {
+        if (typeof i[j][k] === "object") {
+          //let obj = Object.values(i[j][k]);
+          //console.log(i[j][k])
+          let res1 = Object.values(i[j][k]);
+          res1.push("special");
+          //console.log(res1.push("special"))
+          return res1;
+        } else {
+          return Object.values(i[j]);
+        }
+      }
+    }
+  }
+});
+// console.log(res.length);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      products: [],
+      groups: groups,
+      res: res,
+      str: ["Regular", "Selling", "type"],
+      display: false
+    };
+  }
+
+    clickHandle = () => {
+      //if(document.getElementByClassNames('show-carousel').style.visibility === 'hidden') {
+        //document.getElementByClassNames('show-carousel').style.display = 'visible';
+      //}
+      if(!this.state.display) {
+        this.setState({ display: true })
+      }
+    }
+
+  /*async componentDidMount() {
+    try {
+      let response = await fetch('https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json', {
+        mode: 'no-cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*',
+        }
+      });
+      if(!response.ok) {
+        throw new Error(response.statusText);
+      }
+      let data = await response().json();
+      console.log(data);
+      this.setState({ 
+        products: data
+      });
+    } catch(err) {
+      console.log(err);
+    }
+  }
+*/
+  render() {
+    //let { display } = this.state;
+    return (
+      <div className="App">
+        {this.state.groups.map((item, index) => (
+          //console.log(item),
+          //(
+          <div key={index}>
+            <a href={item.links.www}>
+              <h3>{item.name}</h3>
+            </a>
+            <span>
+              <img
+                src={item.hero.href}
+                alt={item.hero.rel}
+                height={item.hero.width}
+                width={item.hero.width}
+                onClick = {this.clickHandle}
+              />
+            </span>
+            <br />
+            <ImageCarousel display={this.state.display} images={item.images} />
+            
+            {this.state.res[index].map((i, index) =>
+              index === 2 ? (
+                <p key={index}>
+                  {this.state.str[index]} - {i}
+                </p>
+              ) : (
+                <p key={index}>
+                  {this.state.str[index]} - ${i}
+                </p>
+              )
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/ImageCarousel.js
+++ src/ImageCarousel.js
@@ -1,1 +1,28 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+
+function ImageCarousel(props) {
+  return (
+    props.display ? 
+    <Carousel >
+      {props.images.map((i, index) => (
+        <a href={i.href} >
+        <img
+          className="d-block w-100"
           src={i.href}
+          //width={i.width} 
+          //height={i.height}
+          alt={i.rel}
+          dataKeyboard="true"
+        />
+        </a>
+      ))}
+    </Carousel>
+    : 
+    null
+  );
+}
+
+export default ImageCarousel;
